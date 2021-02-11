c0f48cb249afacdb8be6f4c082e63fea1692f684
Ruma Das <rumaadl16@gmail.com>
2021-02-11 23:44:18 +1030

    addding style for product

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,6 +1,10 @@
 import { Router } from '@angular/router';
 import { ProductService } from './../../services/product.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  ProductModelServer,
+  ServerResponse,
+} from '../../models/product.module';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  products: any[] = [];
+  products: ProductModelServer[] = [];
   constructor(private productService: ProductService, private router: Router) {}
 
   ngOnInit(): void {
-    this.productService
-      .getAllProducts()
-      .subscribe((prods: { count: Number; products: any[] }) => {
-        this.products = prods.products;
-        console.log(this.products);
-      });
+    this.productService.getAllProducts().subscribe((prods: ServerResponse) => {
+      this.products = prods.products;
+      console.log(this.products);
+    });
   }
   selectProduct(id: Number) {
     this.router.navigate(['/product', id]).then(); //take on that page
