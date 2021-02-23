f04addcd12f3acdeb7c8a9c9b771f4711593f760
Ruma Das <rumaadl16@gmail.com>
2021-02-24 00:42:56 +1030

    adding cart page

--- frontend/src/app/components/cart/cart.component.ts
+++ frontend/src/app/components/cart/cart.component.ts
@@ -1,15 +1,26 @@
+import { CartService } from './../../services/cart.service';
+import { CartModelServer } from './../../models/cart.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartData: CartModelServer;
+  cartTotal: number;
+  subtotal: number;
 
-  constructor() { }
+  constructor(public cartService: CartService) {}
 
   ngOnInit(): void {
+    this.cartService.cartData$.subscribe((data: CartModelServer) => {
+      this.cartData = data;
+    });
+    this.cartService.cartTotal$.subscribe((total) => (this.cartTotal = total));
   }
-
+  ChangeQuantity(id: Number, increase: boolean) {
+    this.cartService.UpdateCartItems(id, increase);
+  }
 }
